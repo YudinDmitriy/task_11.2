e4643a3572cbbf334c27c3438fc9b7fc7276067d
YudinDmitriy <156097092+YudinDmitriy@users.noreply.github.com>
2024-01-15 03:34:27 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -3,7 +3,3 @@ def upper_func(str):
     up_str = str.upper()
     return up_str
 
-def title_func(str):
-    """capitalizes the first letters of the text"""
-    tit_str = str.title()
-    return tit_str
